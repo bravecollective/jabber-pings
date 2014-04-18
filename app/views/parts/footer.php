e372ecffbb0f166dfd872c21fe601574602a62f4
Matthew Glinski <matthewglinski@gmail.com>
2014-04-18 01:23:16 -0400

    Finished modal display of logged pings

--- app/views/parts/footer.php
+++ app/views/parts/footer.php
@@ -51,35 +51,5 @@
 
 
 		$('[data-toggle=tooltip]').tooltip();
-		$('.deleteButton').on('click', function(e)
-		{
-			e.preventDefault();
-			var link = $(this).attr('href');
-
-			// confirm dialog
-			alertify.confirm("Are you sure you want to delete this?", function (ee)
-			{
-				if (ee)
-				{
-					// user clicked confirm
-					window.location = link;
-				}
-				else
-				{
-					// user clicked "cancel", do nothing
-				}
-			});
-
-			return false;
-		});
-
-		jQuery.timeago.settings.allowFuture = true;
-		jQuery(".timeago").timeago();
-
-		setInterval(function()
-		{
-			var utcDate = new Date().toUTCString();
-			$('.time-now').html(utcDate);
-		}, 1000);
 	});
 </script>
