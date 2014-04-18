58b74fbce156a94b1087ab4f37035fe5dbea6635
Matthew Glinski <matthewglinski@gmail.com>
2014-04-18 01:20:59 -0400

    Updated formatting line

--- app/views/parts/footer.php
+++ app/views/parts/footer.php
@@ -6,9 +6,50 @@
 		</p>
 	</div>
 </div>
-<script>
+
+<div class="modalBuilder" style="display: none">
+	<!-- Modal -->
+	<div class="modal fade" id="pingModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title" id="myModalLabel">Ping: <span class="pingUser"></span>, <span class="pingTime"></span></h4>
+				</div>
+				<div class="modal-body">
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+
 	$(document).ready(function()
 	{
+
+		$('.viewPing').on('click', function()
+		{
+			var tr = $(this).parents('tr');
+
+			var user = tr.children('td:eq(0)').html();
+			var time = tr.children('td:eq(1)').html();
+			var msg = tr.children('td:eq(2)').html();
+
+			$('.pingUser', '.modalBuilder .modal').html(user);
+			$('.pingTime', '.modalBuilder .modal').html(time);
+			$('.modal-body', '.modalBuilder .modal').html(msg);
+
+			$('#pingModal').modal({
+				keyboard: false
+			});
+		});
+
+
 		$('[data-toggle=tooltip]').tooltip();
 		$('.deleteButton').on('click', function(e)
 		{
