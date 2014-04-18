b193977718a57cf8386854b26e1dec6ce5194ebd
Matthew Glinski <matthewglinski@gmail.com>
2014-04-17 23:46:27 -0400

    Updated API Auth

--- app/models/APIUser/ApiUserProvider.php
+++ app/models/APIUser/ApiUserProvider.php
@@ -40,6 +40,9 @@ class ApiUserProvider implements UserProviderInterface {
 		}
 		catch(Exception $e)
 		{
+			echo "\n\n";
+			echo $e->getMessage();
+			echo "\n\n";
 			echo "Credentials:\n";
 			var_dump($credentials);
 			echo "\n\n";
